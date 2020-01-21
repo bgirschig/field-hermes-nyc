10f974f3a60c9c7a620b001db0a7eb9048339d75
bastien girschig <bastien.girschig@gmail.com>
2020-01-21 22:15:49 +0100

    Actually solve postProcessing material serialization issue

--- Assets/post_processing/postProcessing.cs
+++ Assets/post_processing/postProcessing.cs
@@ -12,9 +12,9 @@ public class postProcessing : MonoBehaviour {
 
     void Start() {
         var allRenderers = (Renderer[])FindObjectsOfType(typeof(Renderer));
-        var matInstance = new Material(material);
+        materialInstance = new Material(material);
         foreach (Renderer renderer in allRenderers) {
-            if (renderer.sharedMaterial == material) renderer.sharedMaterial = matInstance;
+            if (renderer.sharedMaterial == material) renderer.sharedMaterial = materialInstance;
         }
     }
 
@@ -23,11 +23,11 @@ public class postProcessing : MonoBehaviour {
 
         float ratio = (float)mainCamera.pixelWidth / mainCamera.pixelHeight;
         if (ratio > 1) {
-            material.SetFloat("width", 1.0f/ratio);
-            material.SetFloat("height", 1.0f);
+            materialInstance.SetFloat("width", 1.0f/ratio);
+            materialInstance.SetFloat("height", 1.0f);
         } else {
-            material.SetFloat("width", 1.0f);
-            material.SetFloat("height", ratio);
+            materialInstance.SetFloat("width", 1.0f);
+            materialInstance.SetFloat("height", ratio);
         }
     }
 
@@ -40,6 +40,6 @@ public class postProcessing : MonoBehaviour {
             OnScreenSizeChanged();
         }
 
-		Graphics.Blit(source, destination, material);
+		Graphics.Blit(source, destination, materialInstance);
 	}
 }
