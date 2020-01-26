f68f3d9698b5782cef5c3222e5c189cbd8f47f7c
bastien girschig <bastien.girschig@gmail.com>
2020-01-26 10:39:17 +0100

    Animation curve travel test

---
+++ Assets/various/tests/animCurveTravel/AnimationController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class AnimationController : MonoBehaviour
+{
+    public PlayableDirector director;
+
+    // Start is called before the first frame update
+    void Start() {
+        // director.Pause();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        director.time = (55 + Time.time + Mathf.Sin(Time.time * 2f)) % director.duration;
+    }
+}
