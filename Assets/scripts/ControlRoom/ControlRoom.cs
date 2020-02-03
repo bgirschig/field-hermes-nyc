17fe95412bd4919c120069388ae84567ce10fe08
bastien girschig <bastien.girschig@gmail.com>
2020-02-03 15:02:27 +0100

    wip control room: live swings state

---
+++ Assets/scripts/ControlRoom/ControlRoom.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+public class SocketService : WebSocketBehavior {
+    protected override void OnMessage (MessageEventArgs e) {
+        Sessions.Broadcast(e.Data);
+    }
+}
+
+public class ControlRoom : MonoBehaviour {
+    private WebSocketServer wssv;
+
+    public List<SwingStatus> swings;
+    WebSocket ws;
+
+    float value = 0;
+
+    void Start() {
+        // socket server
+        wssv = new WebSocketServer("ws://0.0.0.0:4649");
+        wssv.AddWebSocketService<SocketService>("/");
+        wssv.Start();
+
+        // Socket client
+        ws = new WebSocket(string.Format("ws://localhost:4649/"));
+        ws.OnMessage += (object sender, MessageEventArgs e) => {
+            value += 1;
+        };
+        ws.Connect();
+    }
+ 
+    void Update() {
+        Debug.Log(value);
+        swings.ForEach(swing => {
+            swing.swingPosition = Mathf.Sin(Time.time * 0.8f);
+            swing.pathPosition = (swing.pathPosition + 0.001f) % 1;
+        });
+    }
+ 
+    void OnApplicationQuit() {
+        ws.Close();
+        wssv.Stop();
+    }
+}
---
+++ Assets/scripts/ControlRoom/SwingStatus.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwingStatus : MonoBehaviour
+{
+    Transform mainSwing;
+    Text text;
+
+    float prevTime = 0;
+    float prevSwingPosition = 0;
+    float prevSpeed = 0;
+    float speed = 0;
+    float min = 0;
+    float max = 0;
+    int swingCount = 0;
+    float dataFramerate = 0;
+
+    float range {
+        get { return max - min; }
+    }
+
+    float _pathPosition = 0;
+    public float pathPosition {
+        get { return _pathPosition; }
+        set {
+            _pathPosition = value;
+            updateDebug();
+        }
+    }
+
+    public float swingPosition {
+        get { return prevSwingPosition; }
+        set {
+            Debug.Log(text);
+            Debug.Log(text.text = "hi there");
+            float now = Time.time;
+            float deltaTime = now - prevTime;
+            dataFramerate = 1/deltaTime;
+            speed = (prevSwingPosition - value) / deltaTime;
+            
+            if (prevSpeed < 0 && speed > 0) swingCount += 1;
+            prevSpeed = speed;
+
+            if (value < min) min = value;
+            if (value > max) max = value;
+
+            prevSwingPosition = value;
+            prevTime = now;
+            mainSwing.rotation = Quaternion.Euler(0, 0, value * 30);
+
+            updateDebug();
+        }
+    }
+
+    void updateDebug() {
+        text.text = string.Format(@"
+<b>SWG</b> {0:+0.00;-0.00}  <b>RNG</b> {1:0.000}
+<b>SPD</b> {2:+0.00;-0.00}  <b>CNT</b> {3:00000}
+<b>FPS</b> {4:00.00}  <b>POS</b> {5:00.0}%
+            ", swingPosition, range, speed, swingCount, dataFramerate, pathPosition*100).Trim();
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        mainSwing = transform.Find("main swing");
+        text = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
