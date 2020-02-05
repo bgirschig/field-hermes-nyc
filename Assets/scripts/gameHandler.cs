9588854f7626bb53fe68dd76d68ce105ecb4fefe
bastien girschig <bastien.girschig@gmail.com>
2020-02-05 22:05:53 +0100

    fix fullscreen resolution bug

--- Assets/scripts/gameHandler.cs
+++ Assets/scripts/gameHandler.cs
@@ -1,8 +1,9 @@
-using UnityEngine;
-
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class gameHandler : MonoBehaviour {
     GameObject UI;
+
     // Start is called before the first frame update
     void Start() {
         Application.targetFrameRate = 30;
@@ -14,9 +15,21 @@ public class gameHandler : MonoBehaviour {
         var ctrl  = (Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftControl));
         var cmd = Input.GetKey(KeyCode.LeftCommand) || Input.GetKey(KeyCode.RightCommand);
         
-        if (ctrl && Input.GetKeyDown(KeyCode.F)) {
+        if (Input.GetKeyDown(KeyCode.F)) {
             Screen.fullScreen = !Screen.fullScreen;
-            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.ExclusiveFullScreen, 60);
+            if (Screen.fullScreen) {
+                Screen.SetResolution(
+                    Screen.currentResolution.width,
+                    Screen.currentResolution.height,
+                    FullScreenMode.FullScreenWindow,
+                    60);
+            } else {
+                Screen.SetResolution(
+                    Screen.currentResolution.width,
+                    Screen.currentResolution.height,
+                    false
+                );
+            }
         }
         if (Input.GetKeyDown(KeyCode.M)) UI.SetActive(!UI.activeSelf);
         if (Input.GetKeyDown(KeyCode.C)) SceneManager.LoadScene("ControlRoom");
