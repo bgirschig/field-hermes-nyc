5bc57349c06a3c519b86f22bfafd8a9255e0f4e0
bastien girschig <bastien.girschig@gmail.com>
2020-02-11 11:37:31 -0500

    one click server start

---
+++ detector2/start_server.sh
@@ -0,0 +1,11 @@
+# install:
+# - put on the dasktop
+# - chmod a+x ~/Desktop/start_server.sh
+# - open with => terminal
+
+# double click to run
+
+cd ~/Desktop/detector
+source env/bin/activate
+
+python server.py
